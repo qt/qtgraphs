e3f8c07d5df43c8f4133871989fd328ea5f86f2b
Tomi Korpipaa <tomi.korpipaa@qt.io>
2023-05-09 11:47:59 +0300

    Clean up qDefaultSurfaceFormat()
    Change-Id: If6190cfe2eb0a67382fe9d10e93abe7f553c861f
    Reviewed-by: Sami Varanka <sami.varanka@qt.io>
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>

--- src/graphs/utils/qutils.h
+++ src/graphs/utils/qutils.h
@@ -5,9 +5,6 @@
 #define QUTILS_H
 
 #include <QtGui/QSurfaceFormat>
-#include <QtGui/QOpenGLContext>
-#include <QtGui/QOpenGLFunctions>
-#include <QtGui/QOffscreenSurface>
 #include <QtCore/QCoreApplication>
 
 QT_BEGIN_NAMESPACE
@@ -15,70 +12,15 @@ QT_BEGIN_NAMESPACE
 [[maybe_unused]]
 static inline QSurfaceFormat qDefaultSurfaceFormat(bool antialias)
 {
-    bool isES = false;
-
     QSurfaceFormat surfaceFormat;
-
-    // Common attributes
     surfaceFormat.setDepthBufferSize(24);
     surfaceFormat.setStencilBufferSize(8);
     surfaceFormat.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
     surfaceFormat.setRenderableType(QSurfaceFormat::DefaultRenderableType);
-
-    QOpenGLContext *ctx = QOpenGLContext::currentContext();
-    QOffscreenSurface *dummySurface = nullptr;
-    if (!ctx) {
-        dummySurface = new QOffscreenSurface();
-        dummySurface->setFormat(surfaceFormat);
-        dummySurface->create();
-        ctx = new QOpenGLContext;
-        ctx->setFormat(surfaceFormat);
-        ctx->create();
-        ctx->makeCurrent(dummySurface);
-    }
-
-#if QT_CONFIG(opengles2)
-    isES = true;
-#elif (QT_VERSION < QT_VERSION_CHECK(5, 3, 0))
-    isES = false;
-#else
-    isES = ctx->isOpenGLES();
-#endif
-
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
-    // We support only ES2 emulation with software renderer for now
-    QString versionStr;
-#ifdef Q_OS_WIN
-    const GLubyte *openGLVersion = ctx->functions()->glGetString(GL_VERSION);
-    versionStr = QString::fromLatin1(reinterpret_cast<const char *>(openGLVersion)).toLower();
-#endif
-    if (versionStr.contains(QStringLiteral("mesa"))
-            || QCoreApplication::testAttribute(Qt::AA_UseSoftwareOpenGL)) {
-        qWarning("Only OpenGL ES2 emulation is available for software rendering.");
-        isES = true;
-    }
-#endif
-
-    if (dummySurface) {
-        ctx->doneCurrent();
-        delete ctx;
-        delete dummySurface;
-    }
-
-    if (isES) {
-        // For ES2 only attributes
-        surfaceFormat.setRedBufferSize(8);
-        surfaceFormat.setBlueBufferSize(8);
-        surfaceFormat.setGreenBufferSize(8);
-    } else {
-        surfaceFormat.setVersion(2, 1);
-        surfaceFormat.setProfile(QSurfaceFormat::CoreProfile);
-        // For OpenGL only attributes
-        if (antialias)
-            surfaceFormat.setSamples(8);
-        else
-            surfaceFormat.setSamples(0);
-    }
+    if (antialias)
+        surfaceFormat.setSamples(8);
+    else
+        surfaceFormat.setSamples(0);
 
     return surfaceFormat;
 }
