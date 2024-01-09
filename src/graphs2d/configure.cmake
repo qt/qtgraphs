d5313f8aa8fdbb3083d95f56743a5134e0f118b6
Tomi Korpipaa <tomi.korpipaa@qt.io>
2024-01-09 12:08:05 +0200

    Add 2D and 3D as configurable features
    This way we can just use Graphs in find_package, but be able to
    use either 3D or 2D, or both, when configuring the module.
    
    Fixes: QTBUG-119920
    Pick-to: 6.7
    Change-Id: I5caa0692587d04492e3e27906d27e2d3d3b7a3cc
    Reviewed-by:  Alexey Edelev <alexey.edelev@qt.io>

--- src/graphs2d/configure.cmake
+++ src/graphs2d/configure.cmake
@@ -1,8 +1,7 @@
 # Copyright (C) 2024 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-qt_feature("graphs-3d" PUBLIC
-    LABEL "3D Graphs"
-    PURPOSE "Support for 3D graphs"
-    CONDITION TARGET Qt6::Quick3D
+qt_feature("graphs-2d" PUBLIC
+    LABEL "2D Graphs"
+    PURPOSE "Support for 2D graphs"
 )
