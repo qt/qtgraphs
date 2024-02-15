447463e5d9cb09d5e8e9edb2200a7ae177340c87
Sakaria Pouke <sakaria.pouke@qt.io>
2024-02-15 09:35:17 +0200

    Fix surface graph wrong label numbers
    Fixes: QTBUG-122247
    Change-Id: I1622bb2fbc869607e66d75f2315d04ed398f2405
    Reviewed-by: Sami Varanka <sami.varanka@qt.io>
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    (cherry picked from commit 749e0086c2cc4b749af8c1dc486cba3d0b7d3786)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/graphs3d/qml/qquickgraphssurface.cpp
+++ src/graphs3d/qml/qquickgraphssurface.cpp
@@ -1892,6 +1892,7 @@ void QQuickGraphsSurface::updateSelectedPoint()
 
         SurfaceVertex selectedVertex;
         selectedVertex.position = pos;
+        selectedVertex.coord = model->selectedVertex.coord;
         if (model->series->isVisible() && !selectedVertex.position.isNull()
             && selectionMode().testFlag(QAbstract3DGraph::SelectionItem)) {
             m_instancing->addPosition(selectedVertex.position);
