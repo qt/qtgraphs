beb21bbfacefac76c0575aee789c57a13ad59dec
Sami Varanka <sami.varanka@qt.io>
2024-05-14 13:25:36 +0300

    Add documentation for xymodelmapper
    Task-number: QTBUG-125314
    Change-Id: I7b4a6591cc12a7c7ebdc8e179a206510bf48a2ca
    Reviewed-by: Kaj Grönholm <kaj.gronholm@qt.io>
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>

--- src/graphs2d/xychart/qxymodelmapper.cpp
+++ src/graphs2d/xychart/qxymodelmapper.cpp
@@ -16,10 +16,9 @@ QT_BEGIN_NAMESPACE
     spline, and scatter series.
 
     Model mappers enable using a data model derived from the QAbstractItemModel
-    class as a data source for a chart. A vertical model mapper is used to
-    create a connection between a line, spline, or scatter series and the data
-    model that has \e X and \e Y columns for the coordinates and holds the data
-    points for the XYSeries as rows. A \e TableModel is a natural choice
+    class as a data source for a graph. A model mapper is used to
+    create a connection between a line, spline, or scatter series.
+    A \e TableModel is a natural choice
     for the model.
 
     Both model and series properties can be used to manipulate the data. The
@@ -36,16 +35,171 @@ QT_BEGIN_NAMESPACE
     \brief A model mapper for XYSeries.
 
     Model mappers enable using a data model derived from the QAbstractItemModel
-    class as a data source for a chart. A vertical model mapper is used to
-    create a connection between a line, spline, or scatter series and the data
-    model that has \e X and \e Y columns for the coordinates and holds the data
-    points for the XYSeries as rows. A \e TableModel is a natural choice
+    class as a data source for a graph. A model mapper is used to
+    create a connection between a line, spline, or scatter series.
+    A \e TableModel is a natural choice
     for the model.
 
     Both model and series properties can be used to manipulate the data. The
     model mapper keeps the series and the data model in sync.
 
    \sa XYSeries
+*/
+
+/*!
+    \property QXYModelMapper::series
+    \brief The series that is used by the mapper.
+
+    All the data in the series is discarded when it is set to the mapper.
+    When a new series is specified, the old series is disconnected (but it
+    preserves its data).
+*/
+/*!
+    \qmlproperty XYSeries XYModelMapper::series
+    The series that is used by the mapper. All the data in the series is
+    discarded when it is set to the mapper. When a new series is specified, the
+    old series is disconnected (but it preserves its data).
+*/
+
+/*!
+    \property QXYModelMapper::model
+    \brief The model that is used by the mapper.
+*/
+/*!
+    \qmlproperty SomeModel XYModelMapper::model
+    The data model that is used by the mapper. You need to implement the model
+    and expose it to QML.
+
+    \note The model has to support adding and removing rows or columns and
+    modifying the data in the cells.
+*/
+
+/*!
+    \property QXYModelMapper::xSection
+    \brief The section of the model that contains the x-coordinates of data
+    points.
+
+    The default value is -1 (invalid mapping).
+
+    \sa QXYModelMapper::orientation
+*/
+/*!
+    \qmlproperty int XYModelMapper::xSection
+    the section of the model that contains the x-coordinates of data points.
+    The default value is -1 (invalid mapping).
+
+    \sa orientation
+*/
+
+/*!
+    \property QXYModelMapper::ySection
+    \brief the section of the model that contains the y-coordinates of data
+    points.
+
+    The default value is -1 (invalid mapping).
+
+    \sa QXYModelMapper::orientation
+*/
+/*!
+    \qmlproperty int XYModelMapper::ySection
+    the section of the model that contains the y-coordinates of data points.
+    The default value is -1 (invalid mapping).
+
+    \sa orientation
+*/
+
+/*!
+    \property QXYModelMapper::first
+    \brief The row of the model that contains the data for the first point
+    of the series.
+
+    The minimum and default value is 0.
+
+    \sa QXYModelMapper::orientation
+*/
+/*!
+    \qmlproperty int XYModelMapper::first
+    The row of the model that contains the data for the first point of the series.
+    The default value is 0.
+
+    \sa orientation
+*/
+
+/*!
+    \property QXYModelMapper::count
+    \brief The number of rows of the model that are mapped as the data for series.
+
+    The minimum and default value is -1 (the number is limited by the number of
+    rows in the model).
+
+    \sa QXYModelMapper::orientation
+*/
+/*!
+    \qmlproperty int XYModelMapper::count
+    The number of rows of the model that are mapped as the data for series. The default value is
+    -1 (the number is limited by the number of rows in the model).
+
+    \sa orientation
+*/
+
+/*!
+    \property QXYModelMapper::orientation
+    \brief Tells the modelmapper how to map data from a model. If
+    \c{Qt::Vertical} is used, each of the model's columns defines a bar set, and the
+    model's rows define the categories. When the orientation is set to
+    \c{Qt::Horizontal}, each of the model's rows defines a bar set, and the model's
+    columns define categories.
+
+    The default value is \c{Qt::Vertical}
+*/
+/*!
+    \qmlproperty orientation XYModelMapper::orientation
+    Tells the modelmapper how to map data from a model. If
+    \c{Qt.Vertical} is used, the model has \e X and \e Y columns, and the
+    model's rows define the data points. When the orientation is set to
+    \c{Qt.Horizontal}, the model has \e X and \e Y rows, and the model's
+    columns define the data points.
+*/
+
+/*!
+    \qmlsignal QXYModelMapper::seriesChanged()
+
+    This signal is emitted when the series that the mapper is connected to changes.
+*/
+
+/*!
+    \qmlsignal XYModelMapper::modelChanged()
+
+    This signal is emitted when the model that the mapper is connected to changes.
+*/
+
+/*!
+    \qmlsignal XYModelMapper::xSectionChanged()
+
+    This signal is emitted when the section that contains the x-coordinates of
+    data points changes.
+*/
+
+/*!
+    \qmlsignal XYModelMapper::ySectionChanged()
+
+    This signal is emitted when the section that contains the y-coordinates of
+    data points changes.
+*/
+
+/*!
+    \qmlsignal XYModelMapper::firstChanged()
+    This signal is emitted when the first row changes.
+*/
+
+/*!
+    \qmlsignal XYModelMapper::countChanged()
+    This signal is emitted when the number of rows changes.
+*/
+
+/*!
+    \qmlsignal BarModelMapper::orientationChanged()
+    This signal is emitted when the orientation changes.
 */
 
 QXYModelMapper::~QXYModelMapper() {}
