1d977ca6b2474bcb4fcd9ef72e2c9a54870aa94f
Alexandru Croitor <alexandru.croitor@qt.io>
2024-05-29 11:37:05 +0200

    CMake: Combine separate configure.cmake files into a single one
    The previous configure.cmake files were evaluated during the build
    to access feature values, but because they were not tied to any
    specific module target, the feature values would not be recorded on
    the exported targets, and then not available to any consumers of the
    Graphs module (which is the case for standalone tests).
    
    To tie the configure.cmake file to a module, it must reside in the
    same directory where the qt_internal_add_module call is done.
    
    Initially there were two separate configure.cmake files because there
    were two separate module targets, but now that they are merged, the
    files should also be merged.
    
    This fixes exporting the features.
    
    Amends 89f36970153d497ebb795108c077bdba97d4f6ed
    Amends d5313f8aa8fdbb3083d95f56743a5134e0f118b6
    
    Pick-to: 6.7 6.8
    Change-Id: I91214d0e7b339f04d157f6eb53879fc88d4424b2
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>

--- src/graphs3d/data/qscatter3dseries.h
+++ src/graphs3d/data/qscatter3dseries.h
@@ -17,7 +17,7 @@ class Q_GRAPHS_EXPORT QScatter3DSeries : public QAbstract3DSeries
     Q_DECLARE_PRIVATE(QScatter3DSeries)
     Q_PROPERTY(QScatterDataProxy *dataProxy READ dataProxy WRITE setDataProxy NOTIFY dataProxyChanged)
     Q_PROPERTY(
-        qsizetype selectedItem READ selectedItem WRITE setSelectedItem NOTIFY selectedItemChanged)
+        int selectedItem READ selectedItem WRITE setSelectedItem NOTIFY selectedItemChanged)
     Q_PROPERTY(float itemSize READ itemSize WRITE setItemSize NOTIFY itemSizeChanged)
     Q_PROPERTY(QScatterDataArray dataArray READ dataArray WRITE setDataArray NOTIFY dataArrayChanged)
 
