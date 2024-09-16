c0ad2d77a304968e1650974c40e48028fa6a54a2
Niko Korkala <niko.korkala@qt.io>
2024-09-16 13:11:14 +0300

    Fix new barsets connection issue and double signaling
    This aims to fix issue with new barsets not creating connection with handleSet
    slots when replace function is used. Also fixes issue of double signal sent
    when setvisibility function is used.
    
    Fixes: QTBUG-128661
    Fixes: QTBUG-128863
    Pick-to: 6.8
    Change-Id: I432349a70032bf4b373142e063a38c46b35688db
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>

--- tests/auto/cpp2dtest/qgbars/tst_bars.cpp
+++ tests/auto/cpp2dtest/qgbars/tst_bars.cpp
@@ -76,6 +76,22 @@ void tst_bars::initializeProperties()
 {
     QVERIFY(m_series);
 
+    QSignalSpy spy0(m_series, &QBarSeries::barWidthChanged);
+    QSignalSpy spy1(m_series, &QBarSeries::labelsVisibleChanged);
+    QSignalSpy spy2(m_series, &QBarSeries::labelsFormatChanged);
+    QSignalSpy spy3(m_series, &QBarSeries::labelsPositionChanged);
+    QSignalSpy spy4(m_series, &QBarSeries::labelsMarginChanged);
+    QSignalSpy spy5(m_series, &QBarSeries::labelsAngleChanged);
+    QSignalSpy spy6(m_series, &QBarSeries::labelsPrecisionChanged);
+    QSignalSpy spy7(m_series, &QBarSeries::seriesColorsChanged);
+    QSignalSpy spy8(m_series, &QBarSeries::borderColorsChanged);
+    QSignalSpy spy9(m_series, &QBarSeries::nameChanged);
+    QSignalSpy spy10(m_series, &QBarSeries::visibleChanged);
+    QSignalSpy spy11(m_series, &QBarSeries::selectableChanged);
+    QSignalSpy spy12(m_series, &QBarSeries::hoverableChanged);
+    QSignalSpy spy13(m_series, &QBarSeries::opacityChanged);
+    QSignalSpy spy14(m_series, &QBarSeries::valuesMultiplierChanged);
+
     auto colors1 = QList<QColor> {QColor(255, 255, 255)};
     auto colors2 = QList<QColor> {QColor(0, 0, 0)};
     auto set = new QBarSet(this);
@@ -115,6 +131,22 @@ void tst_bars::initializeProperties()
     QCOMPARE(m_series->isHoverable(), true);
     QCOMPARE(m_series->opacity(), 0.5);
     QCOMPARE(m_series->valuesMultiplier(), 0.5);
+
+    QCOMPARE(spy0.size(), 1);
+    QCOMPARE(spy1.size(), 1);
+    QCOMPARE(spy2.size(), 1);
+    QCOMPARE(spy3.size(), 1);
+    QCOMPARE(spy4.size(), 1);
+    QCOMPARE(spy5.size(), 1);
+    QCOMPARE(spy6.size(), 1);
+    QCOMPARE(spy7.size(), 1);
+    QCOMPARE(spy8.size(), 1);
+    QCOMPARE(spy9.size(), 1);
+    QCOMPARE(spy10.size(), 1);
+    QCOMPARE(spy11.size(), 1);
+    QCOMPARE(spy12.size(), 1);
+    QCOMPARE(spy13.size(), 1);
+    QCOMPARE(spy14.size(), 1);
 }
 
 void tst_bars::invalidProperties()
@@ -138,6 +170,15 @@ void tst_bars::modifySeries()
 {
     QVERIFY(m_series);
 
+    QSignalSpy spy0(m_series, &QBarSeries::barsetsAdded);
+    QSignalSpy spy1(m_series, &QBarSeries::barsetsReplaced);
+    QSignalSpy spy2(m_series, &QBarSeries::barsetsRemoved);
+    QSignalSpy spy3(m_series, &QBarSeries::countChanged);
+    QSignalSpy spy4(m_series, &QBarSeries::setValueChanged);
+    QSignalSpy spy5(m_series, &QBarSeries::setValueAdded);
+    QSignalSpy spy6(m_series, &QBarSeries::setValueRemoved);
+    QSignalSpy spy7(m_series, &QBarSeries::barSetsChanged); // unused
+
     auto set1 = new QBarSet("Set 1", m_series);
     auto set2 = new QBarSet("Set 2", m_series);
     auto set3 = new QBarSet("Set 3", m_series);
@@ -147,6 +188,7 @@ void tst_bars::modifySeries()
     auto set7 = new QBarSet("Set 7", m_series);
     auto newSet = new QBarSet("New Set", m_series);
     auto pointerBarset = new QBarSet("Set pointer", m_series);
+    auto insertSet = new QBarSet("insertSet", m_series);
 
     // append
     m_series->append(set1);
@@ -166,6 +208,8 @@ void tst_bars::modifySeries()
     QCOMPARE(sets[4], set5);
     QCOMPARE(sets[5], set6);
     QCOMPARE(sets[6], set7);
+    QCOMPARE(spy0.size(), 7);
+    QCOMPARE(spy3.size(), 7);
 
     // pointer remove
     m_series->remove(set3);
@@ -176,6 +220,8 @@ void tst_bars::modifySeries()
     QCOMPARE(sets[3], set5);
     QCOMPARE(sets[4], set6);
     QCOMPARE(sets[5], set7);
+    QCOMPARE(spy2.size(), 1);
+    QCOMPARE(spy3.size(), 8);
 
     // index remove
     QVERIFY(!m_series->remove(-1));
@@ -187,6 +233,8 @@ void tst_bars::modifySeries()
     QCOMPARE(sets[2], set5);
     QCOMPARE(sets[3], set6);
     QCOMPARE(sets[4], set7);
+    QCOMPARE(spy2.size(), 2);
+    QCOMPARE(spy3.size(), 9);
 
     // index replace
     m_series->replace(2, newSet);
@@ -196,6 +244,7 @@ void tst_bars::modifySeries()
     QCOMPARE(sets[2], newSet);
     QCOMPARE(sets[3], set6);
     QCOMPARE(sets[4], set7);
+    QCOMPARE(spy1.size(), 1);
 
     // multiple remove
     m_series->removeMultiple(-1, -1);
@@ -206,6 +255,8 @@ void tst_bars::modifySeries()
     QCOMPARE(sets[0], set1);
     QCOMPARE(sets[1], set6);
     QCOMPARE(sets[2], set7);
+    QCOMPARE(spy2.size(), 5);
+    QCOMPARE(spy3.size(), 12);
 
     // pointer replace
     QVERIFY(!m_series->replace(nullptr, nullptr));
@@ -215,6 +266,7 @@ void tst_bars::modifySeries()
     QCOMPARE(sets[0], set1);
     QCOMPARE(sets[1], pointerBarset);
     QCOMPARE(sets[2], set7);
+    QCOMPARE(spy1.size(), 2);
 
     //at
     auto atSet1 = m_series->at(2);
@@ -238,6 +290,8 @@ void tst_bars::modifySeries()
     // take
     QVERIFY(m_series->take(pointerBarset));
     QCOMPARE(m_series->count(), 2);
+    QCOMPARE(spy2.size(), 7);
+    QCOMPARE(spy3.size(), 14);
 
     // full replace
     QList<QBarSet *> bars = {new QBarSet("new set 1", m_series),
@@ -248,6 +302,38 @@ void tst_bars::modifySeries()
     QCOMPARE(sets[0], bars[0]);
     QCOMPARE(sets[1], bars[1]);
     QCOMPARE(sets[2], bars[2]);
+
+    //append value to barset
+    for (qsizetype i = 0; i < m_series->barSets().size(); ++i)
+        m_series->barSets().at(i)->append(10);
+    QCOMPARE(spy5.size(), 3);
+
+    //replace value from barset
+    for (qsizetype i = 0; i < m_series->barSets().size(); ++i)
+        m_series->barSets().at(i)->replace(0, 20);
+    QCOMPARE(spy4.size(), 3);
+
+    //remove value from barset
+    for (qsizetype i = 0; i < m_series->barSets().size(); ++i)
+        m_series->barSets().at(i)->remove(0);
+    QCOMPARE(spy6.size(), 3);
+
+    //insert
+    m_series->insert(0, insertSet);
+    sets = m_series->barSets();
+    QCOMPARE(sets[0], insertSet);
+
+    m_series->barSets().at(0)->append(10);
+    m_series->barSets().at(0)->replace(0, 20);
+    m_series->barSets().at(0)->remove(0);
+
+    QCOMPARE(spy0.size(), 8);
+    QCOMPARE(spy1.size(), 3);
+    QCOMPARE(spy2.size(), 9);
+    QCOMPARE(spy3.size(), 17);
+    QCOMPARE(spy4.size(), 4);
+    QCOMPARE(spy5.size(), 4);
+    QCOMPARE(spy6.size(), 4);
 }
 
 QTEST_MAIN(tst_bars)
