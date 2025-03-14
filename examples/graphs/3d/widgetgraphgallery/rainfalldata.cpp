6618375f3b95a80c656c3cede0d0e97a162d871a
Niko Korkala <niko.korkala@qt.io>
2025-03-14 12:57:40 +0200

    mark rainfallData.cpp as safety critical file
    reasoning: we are reading a text file from disk without doing any sanity checks
    
    Task-number: QTBUG-127255
    Change-Id: Ie911841541c7423b039a733091eaa65d21810b16
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    (cherry picked from commit 2ffc84aea438c0792f7ef676f75a00696a4f6f85)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 96c0d88ff469f90a2ad8002e8aa803ca06e38751)

--- examples/graphs/3d/widgetgraphgallery/rainfalldata.cpp
+++ examples/graphs/3d/widgetgraphgallery/rainfalldata.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// Qt-Security score:critical reason:data-parser
 
 #include "rainfalldata.h"
 #include <QtCore/qfile.h>
