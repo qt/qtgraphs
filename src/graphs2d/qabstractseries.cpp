526104c3add1a9b26f2a428806759337fde6773e
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-16 16:51:09 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json and REUSE.toml accordingly.
    
    Task-number: QTBUG-136484
    Change-Id: I3522ef96684764ce04998fd2bd02a6abeeeeb95d
    Reviewed-by: Sami Varanka <sami.varanka@qt.io>

--- src/graphs2d/qabstractseries.cpp
+++ src/graphs2d/qabstractseries.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2023 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #include <QtGraphs/qabstractseries.h>
 #include <private/qabstractseries_p.h>
--- src/graphs3d/widget/q3dsurfacewidgetitem.cpp
+++ src/graphs3d/widget/q3dsurfacewidgetitem.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2023 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #include <QtGraphsWidgets/q3dsurfacewidgetitem.h>
 #include <private/q3dsurfacewidgetitem_p.h>
