6b6f6581e25d1f2ea2dd541d92f294b1981445bb
Tomi Korpipaa <tomi.korpipaa@qt.io>
2025-07-09 12:38:14 +0300

    Get rid of some clazy warnings in graphs2d/qsgrenderer folder
    Fix "c++11 range-loop might detach Qt container" warnings.
    
    Pick-to: 6.9 6.10
    Change-Id: I6cfcff806ce6f2f27b4e2037106f72065bafbfcd
    Reviewed-by: Jere Tuliniemi <jere.tuliniemi@qt.io>

--- src/graphs2d/qsgrenderer/barsrenderer.cpp
+++ src/graphs2d/qsgrenderer/barsrenderer.cpp
@@ -256,10 +256,11 @@ void BarsRenderer::updateValueLabels(QBarSeries *series)
 void calculateCategoryTotalValues(QBarSeries *series, QList<float> &totalValues, qsizetype valuesPerSet)
 {
     totalValues.fill(0, valuesPerSet);
-    for (auto s : series->barSets()) {
+    auto barsets = series->barSets();
+    for (auto s : std::as_const(barsets)) {
         QVariantList v = s->values();
         int setIndex = 0;
-        for (auto variantValue : std::as_const(v)) {
+        for (const auto &variantValue : std::as_const(v)) {
             if (setIndex < totalValues.size())
                 totalValues[setIndex] += variantValue.toReal();
             setIndex++;
@@ -309,7 +310,8 @@ void BarsRenderer::updateVerticalBars(QBarSeries *series, qsizetype setCount,
     int barIndexInSet = 0;
     int barSetIndex = 0;
     QList<QLegendData> legendDataList;
-    for (auto s : series->barSets()) {
+    auto barsets = series->barSets();
+    for (auto s : std::as_const(barsets)) {
         QVariantList v = s->values();
         qsizetype valuesCount = v.size();
         if (valuesCount == 0)
@@ -340,7 +342,7 @@ void BarsRenderer::updateVerticalBars(QBarSeries *series, qsizetype setCount,
         color.setAlpha(color.alpha() * series->opacity());
         borderColor.setAlpha(borderColor.alpha() * series->opacity());
         const auto selectedBars = s->selectedBars();
-        for (auto variantValue : std::as_const(v)) {
+        for (const auto &variantValue : std::as_const(v)) {
             const float realValue = variantValue.toReal();
             float value = (realValue - axisY.minValue) * series->valuesMultiplier();
             if (percent) {
@@ -425,7 +427,8 @@ void BarsRenderer::updateHorizontalBars(QBarSeries *series, qsizetype setCount,
     int barIndexInSet = 0;
     int barSetIndex = 0;
     QList<QLegendData> legendDataList;
-    for (auto s : series->barSets()) {
+    auto barsets = series->barSets();
+    for (auto s : std::as_const(barsets)) {
         QVariantList v = s->values();
         qsizetype valuesCount = v.size();
         if (valuesCount == 0)
@@ -455,7 +458,7 @@ void BarsRenderer::updateHorizontalBars(QBarSeries *series, qsizetype setCount,
         color.setAlpha(color.alpha() * series->opacity());
         borderColor.setAlpha(borderColor.alpha() * series->opacity());
         const auto selectedBars = s->selectedBars();
-        for (auto variantValue : std::as_const(v)) {
+        for (const auto &variantValue : std::as_const(v)) {
             const float realValue = variantValue.toReal();
             float value = (realValue - axisX.minValue) * series->valuesMultiplier();
             if (percent) {
