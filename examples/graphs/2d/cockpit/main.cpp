50b20aa2b838d9967f7b87fbad60bf4515ab7219
Friedemann Kleint <Friedemann.Kleint@qt.io>
2025-12-17 12:18:51 +0100

    2D Examples: Remove extra import paths
    This is apparently a legacy from QtCharts.
    
    Pick-to: 6.11
    Change-Id: I0d5b177e2ae70f3580400f62578c7f9ceb86423a
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>

--- examples/graphs/2d/cockpit/main.cpp
+++ examples/graphs/2d/cockpit/main.cpp
@@ -11,15 +11,6 @@ int main(int argc, char *argv[])
 
     QQuickView viewer;
 
-// The following are needed to make examples run without having to install the
-// module in desktop environments.
-#ifdef Q_OS_WIN
-    QString extraImportPath(QStringLiteral("%1/../../../../%2"));
-#else
-    QString extraImportPath(QStringLiteral("%1/../../../%2"));
-#endif
-    viewer.engine()->addImportPath(
-        extraImportPath.arg(QGuiApplication::applicationDirPath(), QString::fromLatin1("qml")));
     viewer.setTitle(QStringLiteral("Cockpit"));
 
     viewer.setMaximumSize({1280, 720});
