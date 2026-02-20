16624ff5c4bae325d5f5b95035a43bd522969f8f
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-02-20 10:49:11 +0100

    graphprinting example: Load QML Module
    Remove the import path fiddling in favor
    of qt6_add_qml_module().
    
    Also remove NO_RESOURCE_TARGET_PATH, which is no longer needed
    and appears to cause issues on macOS.
    
    Change-Id: I3c8fdc08ba5ed076c9f86ea17c606a58d536dcb4
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit b2ecfe7ff73d20e18cb0f13976909d73fcca1063)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/graphs/common/graphprinting/main.cpp
+++ examples/graphs/common/graphprinting/main.cpp
@@ -7,20 +7,13 @@
 #include <QtQuick/qquickview.h>
 #include "graphprinter.h"
 
+using namespace Qt::StringLiterals;
+
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
 
   QQuickView viewer;
 
-// The following are needed to make examples run without having to install the
-// module in desktop environments.
-#ifdef Q_OS_WIN
-  QString extraImportPath(QStringLiteral("%1/../../../../%2"));
-#else
-  QString extraImportPath(QStringLiteral("%1/../../../%2"));
-#endif
-  viewer.engine()->addImportPath(extraImportPath.arg(
-      QGuiApplication::applicationDirPath(), QString::fromLatin1("qml")));
   viewer.setTitle(QStringLiteral("Graph Printing"));
 
   //! [0]
@@ -29,7 +22,7 @@ int main(int argc, char *argv[]) {
   //! [0]
 
   viewer.setMinimumSize({1280, 720});
-  viewer.setSource(QUrl("qrc:/qml/graphprinting/main.qml"));
+  viewer.loadFromModule(u"GraphPrintingExample"_s, u"Main"_s);
   viewer.setResizeMode(QQuickView::SizeRootObjectToView);
   viewer.setColor("white");
   viewer.show();
