d67aa10bfe1099a9558e6739767d77228100b6d2
Sze Howe Koh <szehowe.koh@gmail.com>
2026-09-04 16:49:18 +0800

    Doc: Add missing \since tags to properties related to pan/zoom
    Amends 3e2f152868405447eef0347452c6136bffa1e6f8
    
    This brings GraphsView up-to-date with ValueAxis which got tagged in
    b047a8fb519e21fcd293ea9263c955b258e7f9be
    
    Task-number: QTBUG-122325
    Change-Id: Id24994013b886374ea2523fae7ba0191772be8e3
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    Reviewed-by: Jerome Pasion <jerome.pasion@qt.io>
    (cherry picked from commit 6b416fe56eaeb8e183f8aec4ddc0336f2a0d3b99)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e7e95d065746bcfab460f38fbb136b89ae21d1c7)

--- src/graphs2d/qgraphsview.cpp
+++ src/graphs2d/qgraphsview.cpp
@@ -1693,6 +1693,7 @@ CustomRenderer *QGraphsView::customRenderer() const
 
 /*!
     \property QGraphsView::zoomStyle
+    \since 6.9
     \brief Zoom style of the GraphsView.
 
     Determines the zoom style of the QGraphsView. Zooming works by
@@ -1701,6 +1702,7 @@ CustomRenderer *QGraphsView::customRenderer() const
 */
 /*!
     \qmlproperty enumeration GraphsView::zoomStyle
+    \since 6.9
     Determines the zoom style of the GraphsView. Zooming works by
     manipulating the ValueAxis zoom property. The default value
     is \c {GraphsView.ZoomStyle.None}.
@@ -1739,6 +1741,7 @@ void QGraphsView::setZoomStyle(ZoomStyle newZoomStyle)
 
 /*!
     \property QGraphsView::panStyle
+    \since 6.9
     \brief Pan style of the GraphsView.
 
     Determines the pan style of the QGraphsView. Panning works by
@@ -1747,6 +1750,7 @@ void QGraphsView::setZoomStyle(ZoomStyle newZoomStyle)
 */
 /*!
     \qmlproperty enumeration GraphsView::panStyle
+    \since 6.9
     Determines the pan style of the GraphsView. Panning works by
     manipulating the pan property of a ValueAxis.
     The default value is \c {GraphsView.PanStyle.None}.
@@ -1774,6 +1778,7 @@ void QGraphsView::setPanStyle(PanStyle newPanStyle)
 
 /*!
     \property QGraphsView::zoomAreaEnabled
+    \since 6.9
     \brief Enables zoom area
 
     Zoom area changes mouse and touch dragging to draw a box determined
@@ -1784,6 +1789,7 @@ void QGraphsView::setPanStyle(PanStyle newPanStyle)
 */
 /*!
     \qmlproperty bool GraphsView::zoomAreaEnabled
+    \since 6.9
     Zoom area changes mouse and touch dragging to draw a box determined
     by \c zoomAreaDelegate. Upon release the graph ValueAxis zoom and pan
     properties are changed so that the view covers only the area intersected
@@ -1808,6 +1814,7 @@ void QGraphsView::setZoomAreaEnabled(bool newZoomAreaEnabled)
 
 /*!
     \property QGraphsView::zoomAreaDelegate
+    \since 6.9
     \brief Zoom area visual delegate
 
     Determines the QML element that is drawn when the user performs a drag
@@ -1815,6 +1822,7 @@ void QGraphsView::setZoomAreaEnabled(bool newZoomAreaEnabled)
 */
 /*!
     \qmlproperty Component GraphsView::zoomAreaDelegate
+    \since 6.9
     Determines the QML element that is drawn when the user performs a drag
     motion to zoom in to an area.
 */
@@ -1845,12 +1853,14 @@ void QGraphsView::setZoomAreaDelegate(QQmlComponent *newZoomAreaDelegate)
 
 /*!
     \property QGraphsView::zoomSensitivity
+    \since 6.9
     \brief Zoom value change sensitivity
 
     Determines how fast zoom value changes while zooming.
 */
 /*!
     \qmlproperty real GraphsView::zoomSensitivity
+    \since 6.9
     Determines how fast zoom value changes while zooming.
 */
 qreal QGraphsView::zoomSensitivity() const
